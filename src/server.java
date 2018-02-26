66d1bbf503fde65daad3331bb88f435c0530ca0f
babameme <buiquybao.c500@gmail.com>
2018-02-26 08:44:59 +0700

    first commit

---
+++ src/server.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class server extends JFrame implements ActionListener{
+    static JTextArea content; //
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
